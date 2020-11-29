f073fd292ad879be2d55ffd1b307e69e68a1f0da
hadi-a-azeez <hadikoduvally@gmail.com>
2020-11-29 22:40:24 +0530

    products page complete

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -9,10 +9,12 @@ const Products: React.FC = () => {
         </div>
         <div className={styles.container}>
             <div className={styles.card}>
-                <img src='https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg' 
+                <img src='https://media.thieve.co/products%2Fp34mJdDonjjGQeITPtKo.jpg?fm=jpg&dpr=1&q=70&w=354&h=354' 
                     alt="product" className={styles.thumbnailImage}/>
                 <div className={styles.details}>
-                    <h1 className={styles.heading}>COD</h1>
+                    <h1 className={styles.cod}>Dapputta</h1>
+                    <h1 className={styles.price}>2999</h1>
+                    <h1 className={styles.stock}>In Stock</h1>
                 </div>
             </div>
         </div>
