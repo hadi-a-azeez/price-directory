8885145a02cb27fd1a14b37181e5216457c324b2
hadi-a-azeez <hadikoduvally@gmail.com>
2020-11-30 17:39:20 +0530

    product detailed

--- src/pages/ProductDetailed.tsx
+++ src/pages/ProductDetailed.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './productDetailed.module.scss';
 
 const ProductDetailed: React.FC = () => {
-    return ( <></> );
+    return (<>
+        <div className={styles.container}>
+            <img src="https://media.thieve.co/products%2Fp34mJdDonjjGQeITPtKo.jpg?fm=jpg&dpr=1&q=70&w=354&h=354" alt="" className={styles.productImage} />
+            <div className={styles.details}>
+                <h1 className={styles.cod}>Dapputta</h1>
+                <h1 className={styles.price}>₹2999</h1>
+                <h1 className={styles.stock}>Stock</h1>
+                <h1 className={styles.size}>Small : 5</h1>
+            </div>
+        </div>
+    </>);
+
 }
- 
+
 export default ProductDetailed;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 import styles from "./products.module.scss"
 
 const Products: React.FC = () => {
-    
-    return ( <> 
+
+    return (<>
         <div className={styles.header}>
             <input type="text" placeholder="Search cod here" className={styles.search}></input>
         </div>
         <div className={styles.container}>
             <div className={styles.card}>
-                <img src='https://media.thieve.co/products%2Fp34mJdDonjjGQeITPtKo.jpg?fm=jpg&dpr=1&q=70&w=354&h=354' 
-                    alt="product" className={styles.thumbnailImage}/>
+                <img src='https://media.thieve.co/products%2Fp34mJdDonjjGQeITPtKo.jpg?fm=jpg&dpr=1&q=70&w=354&h=354'
+                    alt="product" className={styles.thumbnailImage} />
+                <div className={styles.details}>
+                    <h1 className={styles.cod}>Dapputta</h1>
+                    <h1 className={styles.price}>2999</h1>
+                    <h1 className={styles.stock}>In Stock</h1>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src='https://media.thieve.co/products%2Fp34mJdDonjjGQeITPtKo.jpg?fm=jpg&dpr=1&q=70&w=354&h=354'
+                    alt="product" className={styles.thumbnailImage} />
+                <div className={styles.details}>
+                    <h1 className={styles.cod}>Dapputta</h1>
+                    <h1 className={styles.price}>2999</h1>
+                    <h1 className={styles.stock}>In Stock</h1>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src='https://media.thieve.co/products%2Fp34mJdDonjjGQeITPtKo.jpg?fm=jpg&dpr=1&q=70&w=354&h=354'
+                    alt="product" className={styles.thumbnailImage} />
                 <div className={styles.details}>
                     <h1 className={styles.cod}>Dapputta</h1>
                     <h1 className={styles.price}>2999</h1>
@@ -18,7 +36,7 @@ const Products: React.FC = () => {
                 </div>
             </div>
         </div>
-    </> );
+    </>);
 }
- 
+
 export default Products;
